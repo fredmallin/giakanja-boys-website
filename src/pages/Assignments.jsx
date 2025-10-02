2def6dccef07c58480ae1cc015d1f5782df77a05
Fredrick Mwangi <fredmallin49@gmail.com>
2025-10-03 00:42:17 +0300

    initialize my project

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -71,8 +71,8 @@ export default function Assignments() {
   };
 
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
-      <h1 className="text-2xl font-bold text-gray-800 mb-6">Assignments</h1>
+    <div className="bg-gray-50 min-h-screen w-full">
+     <h1 className="text-2xl font-bold text-gray-800 mb-6">Assignments</h1>
 
       {Object.keys(assignments).map((form) => (
         <div key={form} className="mb-3">
